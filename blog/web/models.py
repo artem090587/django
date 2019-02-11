52838a4b182254c7035a733185e1e05abbffe7fc
artem090587 <0994210902a@gmail.com>
2019-02-11 11:58:10 +0200

    Models created

--- blog/web/models.py
+++ blog/web/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Article(models.Model):
+    title = models.CharField(max_lenght = 100)
+    content = models.TextField
+    pub_date = models.DateField('data published')
+
+    def __str__(self):
+        return self.title
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Category(models.Model):
+    title = models.CharField(max_lenght = 100)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_lenght = 20)
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    pass
+
+
+class MainMenu(models.Model):
+    pass
+
+
+class Sort(models.Model):
+    pass
+
+
+class Search(models.Model):
+    pass
