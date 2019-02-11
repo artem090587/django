ddb9f785d3300cfffa0a9d888ad2238cbb694b6f
artem090587 <0994210902a@gmail.com>
2019-02-11 13:10:05 +0200

    Templates and Static was created

--- blog/web/models.py
+++ blog/web/models.py
@@ -1,25 +1,66 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver
+from django.core.mail import send_mail
 
-class Article(models.Model):
-    title = models.CharField(max_lenght = 100)
-    content = models.TextField
-    pub_date = models.DateField('data published')
+
+class Blog(models.Model):
+    user = models.OneToOneField(User, primary_key=True, verbose_name='Пользователь')
+    subscribers = models.ManyToManyField(User, blank=True, verbose_name='Подписчики', related_name='blogs')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+
+
+class Post(models.Model):
+    blog = models.ForeignKey(Blog, verbose_name='Блог')
+    title = models.CharField('Заголовок', max_length=255)
+    text = models.TextField('Текст')
+    created = models.DateTimeField('время создания', auto_now_add=True, db_index=True)
+    readers = models.ManyToManyField(User, verbose_name='Кто прочитал', blank=True)
 
     def __str__(self):
         return self.title
-    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ('-created',)
 
 
-class Category(models.Model):
-    title = models.CharField(max_lenght = 100)
+@receiver(post_save, sender=User)
+def signal_user(sender, instance, created, **kwargs):
+    if created:
+        Blog.objects.create(user=instance)
 
-    def __str__(self):
-        return self.title
+
+@receiver(post_save, sender=Post)
+def signal_post(sender, instance, created, **kwargs):
+    if not created:
+        return
+    emails = instance.blog.subscribers.values_list('email', flat=True)
+    username = instance.blog.user.username
+    for email in emails:
+        if not email:
+            continue
+        send_mail('Новый пост от {}'.format(username),
+                  'Новый пост: {}'.format(instance.title),
+                  'noreply@space.com',
+                  [email]
+                  )
+
+
+@receiver(m2m_changed, sender=Blog.subscribers.through)
+def signal_blog(sender, instance, action, **kwargs):
+    if not action == 'post_remove':
+        return
+    pk_set = kwargs.pop('pk_set')
+    Post.readers.through.objects.filter(post__blog_id=instance.user_id, user_id__in=pk_set).delete()
 
 
 class Tag(models.Model):
